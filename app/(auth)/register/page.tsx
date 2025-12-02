ffbfe4cf7cca3a54f9ad2893ee1280d03f41a40e
mac <mac@macs-MacBook-Pro.local>
2025-12-02 22:36:35 +0100

    update

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,22 +1,10 @@
 "use client";
-import { log } from "node:console";
-import React, { useState } from "react";
-import { GiPartyPopper } from "react-icons/gi";
-import { AlertDialogDemo } from "./alert";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
+
 import Loader from "@/app/(pages)/loader";
+import { Divide } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+import { GiPartyPopper } from "react-icons/gi";
 
 const Page = () => {
   const [fullName, setFullName] = useState("");
@@ -39,140 +27,149 @@ const Page = () => {
     }, 4000);
   };
   return (
-    <div className="w-full h-screen p-10">
-      <div className="m-5 flex justify-center items-center">
-        {check ? (
-          <div
-            className="top-[0.5%] p-3"
-            style={{
-              width: "100%",
-              height: "100%",
-              position: "absolute",
-              background: "rgba(255, 255, 255, 0.2)",
-              boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-              backdropFilter: "blur(5px)",
-              WebkitBackdropFilter: "blur(5px)",
-              border: "1px solid rgba(255, 255, 255, 1)",
-            }}
-          >
-            <div className="flex justify-center items-center h-full">
-              <div className="w-[300px] min-h-[250px] flex flex-col gap-5 justify-center items-center rounded-md border border-neutral-300 p-2">
+    <div className="bg-[#080808] py-5 px-10 flex flex-col gap-[50px]">
+      <h1 className="text-white">LOGO</h1>
+      <div className="w-full h-screen">
+        <div className="m-5 flex justify-center items-center">
+          {check ? (
+            <div
+              className="top-[0.5%] p-3"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                background: "rgba(8, 8, 8, 0.61)",
+                boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                backdropFilter: "blur(6.5px)",
+                WebkitBackdropFilter: "blur(6.5px)",
+                border: "1px solid rgba(8, 8, 8, 0.08)",
+              }}
+            >
+              <div className="flex justify-center items-center h-full">
+                <div className="w-[300px] min-h-[250px] flex flex-col gap-5 justify-center items-center rounded-md border border-neutral-300 p-2">
+                  <div className="flex flex-col gap-1 text-white">
+                    <h1 className="text-[20px] font-semibold text-center">
+                      Your Information has been successfully registered
+                    </h1>
+                    <p className="text-[15px] text-red-400">
+                      Note: Contact your agent for your unique access token,
+                      you'd need it to access your dashboard
+                    </p>
+                  </div>
+                  <Link href={"/signin"}>
+                    <button className="p-2  rounded-md w-[180px] text-[18px] bg-[#e4e494] text-black">
+                      Sign In
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div className="text-white flex flex-col gap-5">
+          <h1 className="text-[25px] font-semibold">Sign Up</h1>
+          <div className=" flex justify-center items-center">
+            <form
+              onSubmit={handleSubmit}
+              action=""
+              className=" w-full min-h-[300px]"
+            >
+              <div className="flex text-neutral-400 flex-col gap-5">
+                <div className="flex flex-col gap-2">
+                  <label>Full Name :</label>
+                  <input
+                    type="text"
+                    value={fullName}
+                    onChange={(e) => {
+                      setFullName(e.target.value);
+                    }}
+                    required
+                    className="border border-neutral-700 outline-none rounded-md p-2 bg-[#0d0d0d]"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <label>Email Address :</label>
+                  <input
+                    type="email"
+                    value={email}
+                    required
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    className="border border-neutral-700 outline-none rounded-md p-2 bg-[#0d0d0d]"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <label>Phone Number :</label>
+                  <input
+                    type="number"
+                    value={phoneNumber}
+                    required
+                    onChange={(e) => {
+                      setPhoneNumber(e.target.value);
+                    }}
+                    className="border border-neutral-700 outline-none rounded-md p-2 bg-[#0d0d0d]"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <label>Occupation :</label>
+                  <input
+                    type="text"
+                    value={occupation}
+                    required
+                    onChange={(e) => {
+                      setOccupation(e.target.value);
+                    }}
+                    className="border border-neutral-700 outline-none rounded-md p-2 bg-[#0d0d0d]"
+                  />
+                </div>
                 <div className="flex flex-col gap-1">
-                  <h1 className="text-[20px] font-semibold">
-                    Your Information has been successfully registered
-                  </h1>
-                  <p className="text-[15px] text-red-400">
-                    Note: Contact your agent for your unique access token, you'd
-                    need it to access your dashboard
-                  </p>
-                </div>
-                <Link href={"/signin"}>
-                  <button className="p-2  rounded-md w-[180px] text-[18px] bg-amber-400 text-white">
-                    Sign In
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-
-      <div className=" flex justify-center items-center">
-        <form
-          onSubmit={handleSubmit}
-          action=""
-          className="rounded-md border w-full min-h-[300px] p-5"
-        >
-          <div className="flex flex-col gap-5">
-            <div className="flex flex-col gap-1">
-              <label>Full Name :</label>
-              <input
-                type="text"
-                value={fullName}
-                onChange={(e) => {
-                  setFullName(e.target.value);
-                }}
-                required
-                className="border outline-none rounded-md p-1"
-                placeholder="Enter your name"
-              />
-            </div>
-
-            <div className="flex flex-col gap-1">
-              <label>Email Address :</label>
-              <input
-                type="email"
-                value={email}
-                required
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                className="border outline-none rounded-md p-1"
-                placeholder="Enter your name"
-              />
-            </div>
-
-            <div className="flex flex-col gap-1">
-              <label>Phone Number :</label>
-              <input
-                type="number"
-                value={phoneNumber}
-                required
-                onChange={(e) => {
-                  setPhoneNumber(e.target.value);
-                }}
-                className="border outline-none rounded-md p-1"
-                placeholder="Enter your name"
-              />
-            </div>
-
-            <div className="flex flex-col gap-1">
-              <label>Occupation :</label>
-              <input
-                type="text"
-                value={occupation}
-                required
-                onChange={(e) => {
-                  setOccupation(e.target.value);
-                }}
-                className="border outline-none rounded-md p-1"
-                placeholder="Enter your name"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label>Gender :</label>
-              <div className="flex items-center gap-5">
-                <div className="flex items-center gap-1">
-                  <label htmlFor="male">Male</label>
-                  <input
-                    type="checkbox"
-                    name="male"
-                    id="1"
-                    className="border outline-none rounded-md p-1"
-                  />
-                </div>
-                <div className="flex items-center gap-1">
-                  <label htmlFor="female">Female</label>
-                  <input
-                    type="checkbox"
-                    name="female"
-                    id="1"
-                    className="border outline-none rounded-md p-1"
-                  />
+                  <label>Gender :</label>
+                  <div className="flex items-center gap-5">
+                    <div className="flex items-center gap-1">
+                      <label htmlFor="male">Male</label>
+                      <input
+                        type="checkbox"
+                        name="male"
+                        id="1"
+                        className="border outline-none rounded-md p-1"
+                      />
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <label htmlFor="female">Female</label>
+                      <input
+                        type="checkbox"
+                        name="female"
+                        id="1"
+                        className="border outline-none rounded-md p-1"
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
 
-          <button
-            type="submit"
-            className="bg-amber-400 mt-5 p-2 w-full flex gap-1 items-center justify-center rounded-full h-[45px] text-[17px] text-white"
-          >
-            {loading ? <Loader /> : "Claim Prize"}
-          </button>
-        </form>
+              <button
+                type="submit"
+                className="bg-[#e4e494] mt-5 p-2 w-full flex gap-1 items-center justify-center font-medium rounded-full text-black h-[45px] text-[17px]"
+              >
+                {loading ? (
+                  <Loader />
+                ) : (
+                  <div className="flex items-center gap-1">
+                    <h1>Claim Prize</h1>
+                    <GiPartyPopper className="text-[20px]" />
+                  </div>
+                )}
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import Loader from "@/app/(pages)/loader";
+import Link from "next/link";
+import { GiPartyPopper } from "react-icons/gi";
+
 const Page = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -7,44 +11,89 @@ const Page = () => {
       console.log("Clicked");
     }, 2000);
   };
+  const check = false;
   return (
-    <div className="w-full h-screen p-10 flex justify-center items-center">
-      <form
-        onSubmit={handleSubmit}
-        action=""
-        className="rounded-md border w-full min-h-[300px] p-5"
-      >
-        <div className="flex flex-col gap-5">
-          <div className="flex flex-col gap-1">
-            <label>Email Address :</label>
-            <input
-              type="text"
-              required
-              className="border outline-none rounded-md p-1"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label>Access Token :</label>
-            <input
-              type="text"
-              required
-              className="border outline-none rounded-md p-1"
-              placeholder="Enter your unique Access Token"
-            />
-            <p className="text-[12px] text-red-400">
-              Note: Kindly contact your agent to get your access token
-            </p>
-          </div>
+    <div className="bg-[#080808] py-5 px-10 flex flex-col gap-[50px]">
+      <h1 className="text-white">LOGO</h1>
+      <div className="w-full h-screen">
+        <div className="m-5 flex justify-center items-center">
+          {check ? (
+            <div
+              className="top-[0.5%] p-3"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                background: "rgba(8, 8, 8, 0.61)",
+                boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                backdropFilter: "blur(6.5px)",
+                WebkitBackdropFilter: "blur(6.5px)",
+                border: "1px solid rgba(8, 8, 8, 0.08)",
+              }}
+            >
+              <div className="flex justify-center items-center h-full">
+                <div className="w-[300px] min-h-[250px] flex flex-col gap-5 justify-center items-center rounded-md border border-neutral-300 p-2">
+                  <div className="flex flex-col gap-1 text-white">
+                    <h1 className="text-[20px] font-semibold text-center">
+                      Your Information has been successfully registered
+                    </h1>
+                    <p className="text-[15px] text-red-400">
+                      Note: Contact your agent for your unique access token,
+                      you'd need it to access your dashboard
+                    </p>
+                  </div>
+                  <Link href={"/signin"}>
+                    <button className="p-2  rounded-md w-[180px] text-[18px] bg-[#e4e494] text-black">
+                      Sign In
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
 
-        <button
-          type="submit"
-          className="bg-amber-400 mt-5 p-2 text-white w-full font-semibold flex gap-1 items-center justify-center rounded-full"
-        >
-          Access Dashboard
-        </button>
-      </form>
+        <div className="text-white flex flex-col gap-5 pt-[100px]">
+          <h1 className="text-[25px] font-semibold">Sign In</h1>
+          <div className=" flex justify-center items-center">
+            <form
+              onSubmit={handleSubmit}
+              action=""
+              className=" w-full min-h-[300px]"
+            >
+              <div className="flex text-neutral-400 flex-col gap-5">
+                <div className="flex flex-col gap-2">
+                  <label>Email Address :</label>
+                  <input
+                    type="email"
+                    className="border border-neutral-700 outline-none rounded-md p-2 bg-[#0d0d0d]"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <label>Access Token :</label>
+                  <input
+                    type="number"
+                    className="border border-neutral-700 outline-none rounded-md p-2 bg-[#0d0d0d]"
+                  />
+                  <p className="text-[12px] text-red-400">
+                    Kindly contact your agent to get your unique access token
+                  </p>
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="bg-[#e4e494] mt-5 p-2 w-full flex gap-1 items-center justify-center font-medium rounded-full text-black h-[45px] text-[17px]"
+              >
+                <h1>Access Dashboard</h1>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
